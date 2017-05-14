2c30a6c296021ba2f39e2768343c4a4e9005a747
Jaharsh Samayan <jaharsh93@gmail.com>
2017-05-13 18:18:16 -0700

    Update max_contiguous_subarray.java

--- max_contiguous_subarray.java
+++ max_contiguous_subarray.java
@@ -13,3 +13,25 @@ public class Solution {
         return max;
     }
 }
+
+
+###################################
+
+public class Solution {
+    public int maxSubArray(int[] A) {
+         int n = A.length;
+        int dp =0 ;//dp[i] means the maximum subarray ending with A[i];
+        dp = A[0];
+        int max = dp;
+        int prev=dp,tem=0;
+        
+        
+        for(int i = 1; i < n; i++){
+            tem = A[i] + (prev > 0 ? prev : 0);
+            prev=tem;
+            max = Math.max(max, prev);
+        }
+        
+        return max;
+    }
+}
